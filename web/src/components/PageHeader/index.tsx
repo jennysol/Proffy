feb6c1b62bd06aac4f03c2f3b4d97130ed65bf7b
jennysol <jennyssoliver@gmail.com>
2020-08-05 12:06:45 -0300

    Finalização do Front-end

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -7,6 +7,7 @@ import backIcon from '../../assets/images/icons/back.svg';
 import './styles.css';
 interface PageHeaderProps {
     title: string;
+    description?: string;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
@@ -21,6 +22,8 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
 
             <div className="header-content">
                 <strong>{props.title}</strong>
+                { props.description && <p>{props.description}</p>}
+                
                 {props.children}
             </div>
       </header>  
